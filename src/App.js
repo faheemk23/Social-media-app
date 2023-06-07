68639acdd12da45b569e18e72bfcea69d262fe86
faheemk23 <faheemwork23@gmail.com>
2023-06-08 03:59:30 +0530

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+
 import "./App.css";
+import {
+  Bookmarks,
+  Explore,
+  Home,
+  Login,
+  Post,
+  Profile,
+  Signup,
+} from "./pages/Pages";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <NavLink to="/">Home</NavLink> ||
+      <NavLink to="/bookmarks">bookmarks</NavLink> ||
+      <NavLink to="/explore">explore</NavLink> ||
+      <NavLink to="/login">login</NavLink> ||
+      <NavLink to="/post/1">post</NavLink> ||
+      <NavLink to="/profile/1">profile</NavLink> ||
+      <NavLink to="/signup">signup</NavLink>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post/:postId" element={<Post />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -12,7 +13,9 @@ makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
