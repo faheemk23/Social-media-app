68ca2e6b38be1598fbadaca738903730c6590652
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:38:30 +0530

    feat: post utilities

--- src/utilities/postsUtilities.js
+++ src/utilities/postsUtilities.js
@@ -1,10 +1,57 @@
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
-export async function getAllPosts(postsDispatch) {
+const encodedToken = localStorage.getItem("token");
+
+export async function getAllPosts(dataDispatch) {
   try {
     const res = await axios.get(`/api/posts`);
-    postsDispatch({ type: "set", payload: res.data.posts });
+    dataDispatch({ type: "set-posts", payload: res.data.posts });
   } catch (e) {
-    console.error(e);
+    console.error(e.message);
+  }
+}
+
+export async function deletePost(postId, dataDispatch) {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`, {
+      headers: { authorization: encodedToken },
+    });
+    dataDispatch({ type: "set-posts", payload: res.data.posts });
+    toast.success("Deleted successfully.");
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+export async function likePost(postId, dataDispatch) {
+  try {
+    const res = await axios.post(
+      `/api/posts/like/${postId}`,
+      {},
+      {
+        headers: { authorization: encodedToken },
+      }
+    );
+    dataDispatch({ type: "set-posts", payload: res.data.posts });
+    toast.success("Liked!");
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+export async function dislikePost(postId, dataDispatch) {
+  try {
+    const res = await axios.post(
+      `/api/posts/dislike/${postId}`,
+      {},
+      {
+        headers: { authorization: encodedToken },
+      }
+    );
+    dataDispatch({ type: "set-posts", payload: res.data.posts });
+    toast.success("Disliked!");
+  } catch (e) {
+    console.error(e.message);
   }
 }
