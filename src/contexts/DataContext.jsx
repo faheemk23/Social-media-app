046401e8c7500d67fd3b516b543ba45525af8f30
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:40:06 +0530

    feat: updated data context

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -1,13 +1,26 @@
-import { createContext, useReducer } from "react";
-import { postsReducer } from "../reducers/postsReducer";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { dataReducer } from "../reducers/dataReducer";
+import { followUser } from "../utilities/userUtilities";
+import { getAllPosts } from "../utilities/postsUtilities";
 
 export const DataContext = createContext();
 
 export function DataProvider({ children }) {
-  const [postsState, postsDispatch] = useReducer(postsReducer, { posts: [] });
+  const [dataState, dataDispatch] = useReducer(dataReducer, {
+    posts: [],
+    bookmarks: [],
+    users: [],
+  });
+
+  useEffect(() => {
+    console.log("render");
+    followUser("054c1f2b-dcf8-44ui44-5555-820d4afc", dataDispatch);
+    followUser("054c1f2b-dcf8-439b-86e7-820d4ac416fc", dataDispatch);
+    getAllPosts(dataDispatch);
+  }, []);
 
   return (
-    <DataContext.Provider value={{ postsState, postsDispatch }}>
+    <DataContext.Provider value={{ dataState, dataDispatch }}>
       {children}
     </DataContext.Provider>
   );
