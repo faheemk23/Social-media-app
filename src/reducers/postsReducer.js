ee793681e5cc459d48d691c68cf43e56ccff5341
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:37:21 +0530

    feat: added data reducer

--- src/reducers/postsReducer.js
+++
@@ -1,8 +0,0 @@
-export function postsReducer(state, action) {
-  switch (action.type) {
-    case "set":
-      return { ...state, posts: action.payload };
-    default:
-      return state;
-  }
-}
