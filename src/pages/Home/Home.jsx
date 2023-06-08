8e843a5e489111c433bce24e7c61c57a55208064
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:39:08 +0530

    feat: home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,34 @@
 import { useContext, useEffect } from "react";
 import "./Home.css";
-import { getAllPosts } from "../../utilities/postsUtilities";
+import { getAllPosts, likePost } from "../../utilities/postsUtilities";
 import { DataContext } from "../../contexts/DataContext";
+import PostCard from "../../components/cards/PostCard/PostCard";
+import { addToBookmark } from "../../utilities/bookmarkUtilities";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export function Home() {
-  const { postsState, postsDispatch } = useContext(DataContext);
-  const userData = JSON.parse(localStorage.getItem("userData")) ?? {};
+  const { dataState } = useContext(DataContext);
+  const { user } = useContext(AuthContext);
 
-  useEffect(() => {
-    getAllPosts(postsDispatch);
-  }, []);
-  console.log(postsState);
+  const currentUser = dataState.users.find(
+    ({ username }) => username === user.username
+  );
+
+  const postsOfFollowed = dataState.posts.filter(
+    ({ username: checkUserName }) =>
+      currentUser?.following.some(({ username }) => username === checkUserName)
+  );
+
   return (
     <div>
       Home
       <h1>User</h1>
       <div>
-        Name: {userData.firstName} {userData.lastName}{" "}
+        Name: {user.firstName} {user.lastName}{" "}
       </div>
       <h1>Posts</h1>
-      {postsState.posts.map(({ _id, content, username }) => (
-        <li key={_id}>
-          {username} : {content}
-        </li>
+      {postsOfFollowed.map((post) => (
+        <PostCard key={post._id} post={post} />
       ))}
     </div>
   );
