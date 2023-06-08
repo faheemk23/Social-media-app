e0b8383a6db0d74bed7ee52b5cd9170883f23c60
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:39:46 +0530

    feat: bookmarks page

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,5 +1,25 @@
+import { useContext, useEffect } from "react";
 import "./Bookmarks.css";
+import { getAllBookmarks } from "../../utilities/bookmarkUtilities";
+import { DataContext } from "../../contexts/DataContext";
+import PostCard from "../../components/cards/PostCard/PostCard";
 
 export function Bookmarks() {
-  return <div>Bookmarks</div>;
+  const { dataState, dataDispatch } = useContext(DataContext);
+
+  useEffect(() => {
+    getAllBookmarks(dataDispatch);
+  }, []);
+
+  const getBookmarkPost = (postId) =>
+    dataState.posts.find(({ _id }) => _id === postId);
+
+  return (
+    <div>
+      <h1>Bookmarks</h1>
+      {dataState.bookmarks.map((postId) => (
+        <PostCard key={postId} post={getBookmarkPost(postId)} inBookmark />
+      ))}
+    </div>
+  );
 }
