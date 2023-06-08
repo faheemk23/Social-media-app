b81fa539c8fc3e54bbc6dbcb3a184accd876b31f
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:39:28 +0530

    feat :  explore page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,5 +1,17 @@
+import { useContext } from "react";
+import { DataContext } from "../../contexts/DataContext";
 import "./Explore.css";
+import PostCard from "../../components/cards/PostCard/PostCard";
 
 export function Explore() {
-  return <div>Explore</div>;
+  const { dataState } = useContext(DataContext);
+
+  return (
+    <div>
+      <h1>Explore</h1>
+      {dataState.posts.map((post) => (
+        <PostCard key={post._id} post={post} />
+      ))}
+    </div>
+  );
 }
