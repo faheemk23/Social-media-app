9d2f69b3f9e39f5794f87522f9111f8d72c7b2dd
faheemk23 <faheemwork23@gmail.com>
2023-06-09 02:40:44 +0530

    feat: temp profile card

---
+++ src/components/cards/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { followUser, unfollowUser } from "../../../utilities/userUtilities";
+import "./ProfileCard.css";
+import { DataContext } from "../../../contexts/DataContext";
+import { AuthContext } from "../../../contexts/AuthContext";
+
+export default function ProfileCard({ user }) {
+  const { user: currentUser } = useContext(AuthContext);
+  const { dataDispatch } = useContext(DataContext);
+  const { _id, firstName, lastName, username, followers, following } = user;
+
+  const isFollowed = () =>
+    user.followers.some(({ username }) => username === currentUser.username);
+
+  return (
+    <div>
+      <h3>
+        {firstName} {lastName}
+      </h3>
+      <h3>@{username}</h3>
+      <p>Followers: {followers.length}</p>
+      <p>Following : {following.length}</p>
+      {username !== "faheemk237" &&
+        (isFollowed() ? (
+          <button onClick={() => unfollowUser(_id, dataDispatch)}>
+            Unfollow
+          </button>
+        ) : (
+          <button onClick={() => followUser(_id, dataDispatch)}>Follow</button>
+        ))}
+    </div>
+  );
+}
