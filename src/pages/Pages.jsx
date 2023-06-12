fd4210b4d5d1883d932e2a62ff275d0824b683ec
faheemk23 <faheemwork23@gmail.com>
2023-06-12 20:42:18 +0530

    feat: landing page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,9 +1,10 @@
 import { Bookmarks } from "./Bookmarks/Bookmarks";
 import { Explore } from "./Explore/Explore";
 import { Home } from "./Home/Home";
+import { Landing } from "./Landing/Landing";
 import { Login } from "./Login/Login";
 import { Post } from "./Post/Post";
 import { Profile } from "./Profile/Profile";
 import { Signup } from "./Signup/Signup";
 
-export { Bookmarks, Explore, Home, Login, Post, Profile, Signup };
+export { Bookmarks, Explore, Home, Login, Post, Profile, Signup, Landing };
