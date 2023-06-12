6292c3287003d7b076cef28865baae694349d8db
faheemk23 <faheemwork23@gmail.com>
2023-06-12 20:43:15 +0530

    refactor: semantic, acc to db

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,15 @@ import { DataContext } from "../../contexts/DataContext";
 import PostCard from "../../components/cards/PostCard/PostCard";
 import { addToBookmark } from "../../utilities/bookmarkUtilities";
 import { AuthContext } from "../../contexts/AuthContext";
+import { NavLink } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
 
 export function Home() {
   const { dataState } = useContext(DataContext);
-  const { user } = useContext(AuthContext);
+
+  const getUser = localStorage.getItem("userData");
+
+  const user = getUser ? JSON.parse(getUser) : {};
 
   const currentUser = dataState.users.find(
     ({ username }) => username === user.username
@@ -20,16 +25,14 @@ export function Home() {
   );
 
   return (
-    <div>
+    <section>
       Home
       <h1>User</h1>
-      <div>
-        Name: {user.firstName} {user.lastName}{" "}
-      </div>
+      <div>Name: {user.name}</div>
       <h1>Posts</h1>
       {postsOfFollowed.map((post) => (
         <PostCard key={post._id} post={post} />
       ))}
-    </div>
+    </section>
   );
 }
