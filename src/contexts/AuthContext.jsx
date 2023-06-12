2a96d622a4864d272e2bd31bfee9378de314bfc1
faheemk23 <faheemwork23@gmail.com>
2023-06-12 20:41:50 +0530

    feat: logged in state

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,17 +1,23 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { DataContext } from "./DataContext";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({});
+  const { users } = useContext(DataContext);
 
   useEffect(() => {
-    if (localStorage.getItem("userData")) {
-      setUser(JSON.parse(localStorage.getItem("userData")));
+    const currentUser = localStorage.getItem("userData");
+    if (currentUser) {
+      setUser(JSON.parse(currentUser));
     }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
