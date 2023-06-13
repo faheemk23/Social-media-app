6e1d43c956787632da4c67b1894e2456a2cba4ea
faheemk23 <faheemwork23@gmail.com>
2023-06-13 06:25:33 +0530

    feat: initial login from local storage

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,22 +1,37 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { DataContext } from "./DataContext";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState({});
   const { users } = useContext(DataContext);
 
   useEffect(() => {
-    const currentUser = localStorage.getItem("userData");
+    const currentUser = JSON.parse(localStorage.getItem("userData"));
+    const initialLoginHandler = async () => {
+      try {
+        const res = await axios.get(`/api/users`);
+        const userInDatabase = res.data.users.some(
+          ({ username }) => username === currentUser.username
+        );
+        if (userInDatabase) {
+          setLoggedIn(true);
+          setUser(currentUser);
+        }
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
     if (currentUser) {
-      setUser(JSON.parse(currentUser));
+      initialLoginHandler();
     }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, loggedIn, setLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
