c1b418904f0374958ec183b3836329f51236573e
faheemk23 <faheemwork23@gmail.com>
2023-06-13 06:26:32 +0530

    feat: navbar based on log in

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,72 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import "./NavBar.css";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function NavBar({ inBottom }) {
+  const { setUser, loggedIn, setLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
   const getActiveStyle = ({ isActive }) =>
-    isActive ? { fontWeight: "bold", color: "red" } : {};
+    isActive ? { fontWeight: "bold" } : {};
+
+  const handleBtnLogout = () => {
+    setLoggedIn(false);
+    setUser({});
+    localStorage.clear();
+    navigate("/explore");
+  };
   return (
     <nav className={inBottom ? "nav nav-bottom" : "nav nav-left"}>
-      <NavLink className="navlink" style={getActiveStyle} to="/home">
-        <i className="fa-solid fa-house"></i>
-        <span className="navlink-text">Home</span>
-      </NavLink>
+      {!inBottom && (
+        <div
+          className="navlink logo"
+          onClick={() => (loggedIn ? navigate("/home") : navigate("/explore"))}
+        >
+          <img
+            className="nav-logo"
+            src="https://ik.imagekit.io/faheem/Social-media/app-logo?updatedAt=1686601318657"
+            alt="app-logo"
+            width="40px"
+            height="32px"
+          />
+        </div>
+      )}
+
+      {loggedIn && (
+        <NavLink className="navlink" style={getActiveStyle} to="/home">
+          <i className="fa-solid fa-house navlink-icon"></i>
+          <span className="navlink-text">Home</span>
+        </NavLink>
+      )}
       <NavLink className="navlink" style={getActiveStyle} to="/explore">
-        <i className="fa-solid fa-magnifying-glass"></i>
+        <i className="fa-solid fa-magnifying-glass navlink-icon"></i>
         <span className="navlink-text">Explore</span>
       </NavLink>
-      <NavLink className="navlink" style={getActiveStyle} to="/bookmarks">
-        <i className="fa-regular fa-bookmark"></i>
-        <span className="navlink-text">Bookmarks</span>
-      </NavLink>
-      <NavLink className="navlink" style={getActiveStyle} to="/profile/1">
-        <i className="fa-regular fa-user"></i>
-        <span className="navlink-text">Profile</span>
-      </NavLink>
-      <button>Tweet</button>
+
+      {!loggedIn && (
+        <NavLink className="navlink" style={getActiveStyle} to="#">
+          <i className="fa-solid fa-gear navlink-icon"></i>
+          <span className="navlink-text">Settings</span>
+        </NavLink>
+      )}
+      {loggedIn && (
+        <>
+          <NavLink className="navlink" style={getActiveStyle} to="/bookmarks">
+            <i className="fa-regular fa-bookmark navlink-icon"></i>
+            <span className="navlink-text">Bookmarks</span>
+          </NavLink>
+          <NavLink className="navlink" style={getActiveStyle} to="/profile/1">
+            <i className="fa-regular fa-user navlink-icon"></i>
+            <span className="navlink-text">Profile</span>
+          </NavLink>
+          {!inBottom && <button>Tweet</button>}
+        </>
+      )}
+      {loggedIn ? (
+        <button onClick={handleBtnLogout}>Logout</button>
+      ) : (
+        <Link to="/login">login</Link>
+      )}
     </nav>
   );
 }
