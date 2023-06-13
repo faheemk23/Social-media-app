4f34fa075a7e9e208604c9fbcc084735115683d7
faheemk23 <faheemwork23@gmail.com>
2023-06-13 06:23:01 +0530

    feat: setting user and log in state in auth func

--- src/utilities/authUtilities.js
+++ src/utilities/authUtilities.js
@@ -1,28 +1,35 @@
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
-export async function loginHandler(userDetail, navigate) {
+export async function loginHandler(userDetail, setUser, navigate, setLoggedIn) {
   try {
     const res = await axios.post("/api/auth/login", userDetail);
-    localStorage.setItem("token", res.data.encodedToken);
-    const { username, name } = res.data.foundUser;
-    localStorage.setItem("userData", JSON.stringify({ username, name }));
-    navigate("/");
-    toast.success("Logged in");
+    if (res.status === 200) {
+      localStorage.setItem("token", res.data.encodedToken);
+      const { username, name } = res.data.foundUser;
+      localStorage.setItem("userData", JSON.stringify({ username, name }));
+      setUser({ username, name });
+      navigate("/home");
+      setLoggedIn(true);
+      toast.success("Logged in");
+    }
   } catch (e) {
     console.error(e);
   }
 }
 
-export async function signupHandler(userDetail, navigate) {
+export async function signupHandler(userDetail, navigate, setLoggedIn) {
   try {
     const res = await axios.post("/api/auth/signup", userDetail);
     console.log({ res });
-    localStorage.setItem("token", res.data.encodedToken);
-    const { username, name } = res.data.createdUser;
-    localStorage.setItem("userData", JSON.stringify({ username, name }));
-    navigate("/");
-    toast.success("Logged in");
+    if (res.status === 201) {
+      localStorage.setItem("token", res.data.encodedToken);
+      const { username, name } = res.data.createdUser;
+      localStorage.setItem("userData", JSON.stringify({ username, name }));
+      navigate("/home");
+      setLoggedIn(true);
+      toast.success("Logged in");
+    }
   } catch (e) {
     console.error(e);
   }
