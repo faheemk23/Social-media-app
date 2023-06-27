83518a0591b49daab0e330d61ae1320b770ed6e9
faheemk23 <faheemwork23@gmail.com>
2023-06-27 16:17:28 +0530

    feat: header and loader

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,25 +1,49 @@
-import { useContext, useEffect } from "react";
-import "./Profile.css";
-import { getAllUsers } from "../../utilities/userUtilities";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { ProfileCard } from "../../components/cards/ProfileCard/ProfileCard";
+import { ProfileHeader } from "../../components/headers/ProfileHeader";
+import { Loader } from "../../components/modals/Loader/Loader";
+import { AuthContext } from "../../contexts/AuthContext";
 import { DataContext } from "../../contexts/DataContext";
-import ProfileCard from "../../components/cards/ProfileCard/ProfileCard";
+import { getUserPosts } from "../../utilities/postsUtilities";
+import { getUser } from "../../utilities/userUtilities";
+import "./Profile.css";
 
 export function Profile() {
   const {
-    dataState: { users },
+    dataState: { users, posts },
     dataDispatch,
   } = useContext(DataContext);
+  const { user } = useContext(AuthContext);
+  const [currentUser, setCurrentUser] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { username } = useParams();
 
   useEffect(() => {
-    getAllUsers(dataDispatch);
-  }, []);
+    setLoading(true);
+    getUser(username, setCurrentUser, setLoading);
+  }, [username, users, user]);
+
+  useEffect(() => {
+    getUserPosts(username, setUserPosts);
+  }, [username, posts]);
 
   return (
     <div>
-      <h1>Profiles</h1>
-      {users.map((user) => (
-        <ProfileCard key={user._id} user={user} />
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <ProfileHeader
+            name={currentUser.name}
+            numberOfPosts={userPosts.length}
+          />
+          <ProfileCard user={currentUser} userPosts={userPosts} />
+        </>
+      )}
     </div>
   );
 }
