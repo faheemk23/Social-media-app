2e476041b22c171adb34dd3c677122c2d9a6e42e
faheemk23 <faheemwork23@gmail.com>
2023-06-27 16:24:03 +0530

    feat : create post modal and showing own posts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { PostCard } from "../../components/cards/PostCard/PostCard";
 import { DataContext } from "../../contexts/DataContext";
-import PostCard from "../../components/cards/PostCard/PostCard";
 
-import "./Home.css";
-import HomeHeader from "../../components/headers/HomeHeader";
 import PostInput from "../../components/PostInput/PostInput";
+import { HomeHeader } from "../../components/headers/HomeHeader";
+import "./Home.css";
 
-export function Home() {
+export function Home({ showCreatePostModal }) {
+  const [filter, setFilter] = useState("");
   const { dataState } = useContext(DataContext);
 
   const getUser = localStorage.getItem("userData");
@@ -26,11 +27,28 @@ export function Home() {
   );
 
   const homePosts = [...postsOfFollowed, ...userPosts];
+
+  let filteredPosts = homePosts;
+
+  // filtering posts
+
+  if (filter === "trending") {
+    filteredPosts = filteredPosts.sort(
+      (a, b) => b.likes.likeCount - a.likes.likeCount
+    );
+  } else if (filter === "latest") {
+    filteredPosts = filteredPosts.sort((a, b) => {
+      const dateA = new Date(a.createdAt);
+      const dateB = new Date(b.createdAt);
+      return dateB - dateA;
+    });
+  }
+
   return (
     <section>
-      <HomeHeader />
-      <PostInput />
-      {homePosts?.map((post) => (
+      <HomeHeader filter={filter} setFilter={setFilter} />
+      {!showCreatePostModal && <PostInput />}
+      {filteredPosts?.map((post) => (
         <PostCard key={post._id} post={post} />
       ))}
     </section>
