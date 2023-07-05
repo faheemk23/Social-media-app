2b4bc6030c5969c92c5ab64cc8a0cc8030051ab4
faheemk23 <faheemwork23@gmail.com>
2023-07-06 02:30:30 +0530

    feat: logout page

---
+++ src/pages/Logout/Logout.jsx
@@ -0,0 +1,10 @@
+import LogoutModal from "../../components/modals/LogoutModal/LogoutModal";
+import "./Logout.css";
+
+export function Logout() {
+  return (
+    <div>
+      <LogoutModal />
+    </div>
+  );
+}
