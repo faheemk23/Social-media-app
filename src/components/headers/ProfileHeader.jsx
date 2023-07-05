d6ca3c7f19ca453592f63e4fa30bd506a6d87b24
faheemk23 <faheemwork23@gmail.com>
2023-07-06 01:43:32 +0530

    feat: added profile header

--- src/components/headers/ProfileHeader.jsx
+++ src/components/headers/ProfileHeader.jsx
@@ -4,13 +4,16 @@ import "./Headers.css";
 export function ProfileHeader({ name, numberOfPosts }) {
   const navigate = useNavigate();
   return (
-    <div className="header-container">
+    <div className="header-container small-header">
       <div className="pointer" onClick={() => navigate(-1)}>
-        <i className="fa-solid fa-arrow-left"></i>
+        <i className="fa-solid fa-arrow-left icon"></i>
       </div>
-
-      <div className="header-heading">{name}</div>
-      <div>{numberOfPosts} tweets</div>
+      <div>
+        <div className="profile-header-name">{name}</div>
+        <div className="profile-header-tweets-number">
+          {numberOfPosts} {numberOfPosts <= 1 ? "tweet" : "tweets"}
+        </div>
+      </div>
     </div>
   );
 }
