d90fe0b69566ff40b048536830770a8fa934294c
faheemk23 <faheemwork23@gmail.com>
2023-07-05 18:46:31 +0530

    feat: login and signup modals

---
+++ src/components/modals/LoginModal/LoginModal.jsx
@@ -0,0 +1,113 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../../contexts/AuthContext";
+import { loginHandler } from "../../../utilities/authUtilities";
+import GuestLoginUsers from "../../GuestLoginUsers/GuestLoginUsers";
+import "./LoginModal.css";
+
+export function LoginModal() {
+  const [userDetail, setUserDetail] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [showGuestUsers, setShowGuestUsers] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { setUser, setLoggedIn } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const loginFieldChangeHandler = (e) => {
+    const fieldChanged = e.target.id;
+    setUserDetail((prev) => ({ ...prev, [fieldChanged]: e.target.value }));
+  };
+
+  const valiateDetails = (userDetail, setErrorMessage) => {
+    const res = Object.keys(userDetail).every((key) => {
+      if (userDetail[key] === "") {
+        const field = key === "confirmPassword" ? "confirm password" : key;
+        setErrorMessage(`Please fill the ${field} field!`);
+        return false;
+      } else {
+        return true;
+      }
+    });
+    return res;
+  };
+
+  const handleBtnLogin = () => {
+    if (valiateDetails(userDetail, setErrorMessage)) {
+      loginHandler(userDetail, setUser, navigate, setLoggedIn);
+    }
+  };
+
+  return (
+    <div className="modal-container ">
+      <div className="login-modal relative">
+        <i
+          className="fa-solid fa-xmark login-modal-close-icon pointer"
+          onClick={() => navigate("/")}
+        ></i>
+        <div className="flex-center">
+          {" "}
+          <img
+            className="nav-logo"
+            src="https://ik.imagekit.io/faheem/Social-media/app-logo?updatedAt=1686601318657"
+            alt="app-logo"
+            width="40px"
+            height="32px"
+          />
+        </div>
+        <h1 className="login-modal-heading">Sign in to Twitter</h1>
+        {errorMessage && <p className="pink">{errorMessage}</p>}
+
+        <div className="flex-column">
+          <label htmlFor="username">Username: </label>
+          <input
+            className="login-modal-input"
+            type="text"
+            id="username"
+            value={userDetail.username}
+            onChange={loginFieldChangeHandler}
+          />
+        </div>
+
+        <div className="flex-column">
+          <label htmlFor="password">Password: </label>
+          <input
+            className="login-modal-input"
+            type="password"
+            id="password"
+            value={userDetail.password}
+            onChange={loginFieldChangeHandler}
+          />
+        </div>
+
+        <button className="btn btn-primary" onClick={handleBtnLogin}>
+          Login
+        </button>
+        {showGuestUsers && (
+          <GuestLoginUsers
+            setUserDetail={setUserDetail}
+            setShowGuestUsers={setShowGuestUsers}
+          />
+        )}
+        <button
+          className="btn btn-secondary"
+          onClick={() => setShowGuestUsers(true)}
+        >
+          Guest Login
+        </button>
+        <div className="login-modal-suggestion">
+          Don't have a account?{" "}
+          <Link className="link" to="/signup">
+            Signup
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
