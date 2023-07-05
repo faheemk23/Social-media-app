cfa7447f28d23691cf6466210c9d1433dbda5c3f
faheemk23 <faheemwork23@gmail.com>
2023-07-06 02:31:08 +0530

    feat: signup ,modal

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,8 +3,19 @@ import { Explore } from "./Explore/Explore";
 import { Home } from "./Home/Home";
 import { Landing } from "./Landing/Landing";
 import { Login } from "./Login/Login";
+import { Logout } from "./Logout/Logout";
 import { Post } from "./Post/Post";
 import { Profile } from "./Profile/Profile";
 import { Signup } from "./Signup/Signup";
 
-export { Bookmarks, Explore, Home, Landing, Login, Post, Profile, Signup };
+export {
+  Bookmarks,
+  Explore,
+  Home,
+  Landing,
+  Login,
+  Logout,
+  Post,
+  Profile,
+  Signup,
+};
